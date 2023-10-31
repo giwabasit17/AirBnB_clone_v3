b7f44c75ebbc8ee6454bc99bf73f1b1770e5166e
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-10-31 07:46:15 -0700

    api

--- models/user.py
+++ models/user.py
@@ -1,25 +1,48 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from models import storage_type
+""" holds class User"""
+import hashlib
+from os import getenv
+from typing import Any
+
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+import models
+from models.base_model import Base, BaseModel
+
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+    """Representation of a user"""
+
+    if models.storage_t == "db":
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        if kwargs:
+            pwd = kwargs.pop("password", None)
+            if pwd:
+                User.__set_password(self, pwd)
+
+        super().__init__(*args, **kwargs)
+
+    def __set_password(self, pwd):
+        """
+        custom setter: encrypts password to MD5
+        """
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        secure_password = secure.hexdigest()
+        setattr(self, "password", secure_password)
